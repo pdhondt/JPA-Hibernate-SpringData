029d7968d71fdc898301bcef5709cc386cd29d1a
Peter D'hondt <dhondt.peter@gmail.com>
2023-03-09 12:26:30 +0100

    hoofdstuk 8 - Enum

--- src/main/java/be/vdab/fietsen/domain/Docent.java
+++ src/main/java/be/vdab/fietsen/domain/Docent.java
@@ -1,8 +1,6 @@
 package be.vdab.fietsen.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 
@@ -16,6 +14,8 @@ public class Docent {
     private BigDecimal wedde;
 
     private String emailAdres;
+    @Enumerated(EnumType.STRING)
+    private Geslacht geslacht;
 
     public long getId() {
         return id;
@@ -35,5 +35,9 @@ public class Docent {
 
     public String getEmailAdres() {
         return emailAdres;
+    }
+
+    public Geslacht getGeslacht() {
+        return geslacht;
     }
 }
